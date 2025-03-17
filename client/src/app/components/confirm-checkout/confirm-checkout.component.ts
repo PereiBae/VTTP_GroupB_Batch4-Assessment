63466cb2bbba334f20cbba883b69f901c103c1c4
PereiBae <pereibae@gmail.com>
2025-03-17 12:35:08 +0800

    Attempting task 3.1

--- client/src/app/components/confirm-checkout/confirm-checkout.component.ts
+++ client/src/app/components/confirm-checkout/confirm-checkout.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-confirm-checkout',
   templateUrl: './confirm-checkout.component.html',
   styleUrl: './confirm-checkout.component.css'
 })
-export class ConfirmCheckoutComponent {
+export class ConfirmCheckoutComponent implements OnInit {
 
   // TODO Task 3
+  protected form!: FormGroup;
+  private fb = inject(FormBuilder)
+
+  ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      'name': this.fb.control<string>('', [Validators.required]),
+      'address': this.fb.control<string>('', [Validators.required, Validators.minLength(3)]),
+      'priority': this.fb.control<boolean>(false, [Validators.required]),
+      'comments': this.fb.control<string>(''),
+    })
+  }
 
 }
